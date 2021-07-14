1f8302464e95a3c4b664f9ea4a41a662082a7273
jonathan_perez <perezgradosj@gmail.com>
2021-07-14 02:30:46 -0500

    cambios

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 window.addEventListener("load", function() {
     let tabs = document.querySelector(".tabs");
     let options = { responsiveThreshold: 100 };
-    let instance = M.Tabs.init(tabs, options);
+    M.Tabs.init(tabs, options);
 })
+
+function newPedidoAlert(){
+    var modal = document.querySelector('#modal1');
+    var instanceModal = M.Modal.init(modal);
+    instanceModal.open();
+}
